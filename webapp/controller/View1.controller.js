d4b116d9914b82030a55dd1f111a7db3e1184e8f
Nandhini2998 <nandhini.g@clss.in>
2026-05-14 10:35:03 +0530

    Add oData and bind with controller
    Add oData and bind with controller

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,18 +7,19 @@ sap.ui.define([
 
     return Controller.extend("projectmisreport.controller.View1", {
 
-        onInit: function () {
-            const oData = Data.getData();
-            const oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
-        },
-        
-        onTilePress: function (oEvent) {
-            const oData = oEvent.getSource()
-                .getBindingContext()
-                .getObject();
+       onInit: function () {
 
-            sap.m.MessageToast.show(oData.title);
-        }
+        const oData = Data.getData();
+        const oModel = new JSONModel(oData);
+        this.getView().setModel(oModel);
+
+        var sProject_id = 'TEST11';
+        var oTable = this.byId("idTable");
+        oTable.setModel(this.getOwnerComponent().getModel());
+        oTable.bindRows({
+            path: `/ZCB_I_PROJECT_MIS1(P_PROJECT='${sProject_id}')/Set`
+        });
+    }
+
     });
 });
